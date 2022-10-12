5a62729195c719f553b3d974e59ba55c8d01b4a5
Henry Chun <chunhyungjoon@gmail.com>
2022-10-12 12:42:36 +1100

    initial flask

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# this sentence is a universal db connection 'protocol+adapter'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
+
+db = SQLAlchemy(app)
+
+print(db)
+
+
+@app.route('/')
+def index():
+    return "Hello, world!"
