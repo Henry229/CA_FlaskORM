7fbe47ea6383e7e8c63338a4983b8bbbede67b58
Henry Chun <chunhyungjoon@gmail.com>
2022-10-18 12:54:59 +1100

    initial JWT

--- app.py
+++ app.py
@@ -2,16 +2,29 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
-
-# from symbol import stmt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    admin = db.Column(db.Boolean, default=False)
+    
+class UserSchema(ma.Schema):
+    class Meta:
+        flields = ('id', 'name', 'email', 'is_admin')
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -22,6 +35,12 @@ class Card(db.Model):
     date = db.Column(db.Date)
     status = db.Column(db.String)
     priority = db.Column(db.String)
+
+
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'status', 'priority', 'date')
+        ordered = True
 
 
 # Define a custom CLI (terminal) command
@@ -39,6 +58,19 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
+    users = [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
+            admin=True
+        ),
+        User(
+            name='Tom',
+            email='someone@spam.com',
+            password=bcrypt.generate_password_hash('12345').decode('utf-8'),
+            admin=False
+        )
+    ]
     cards = [
         Card(
             title='Start the project',
@@ -71,38 +103,38 @@ def seed_db():
     ]
 
     db.session.add_all(cards)
+    db.session.add_all(users)
     db.session.commit()
     print('Tables seeded')
 
 
-@app.cli.command('/cards/')
+@app.route('/cards/')
 def all_cards():
     # select * from cards;
     # cards = Card.query.all()
-    # stmt = db.select(Card).where(db.or_
-    #                              (Card.status == 'To do', Card.priority == 'High'))
-    stmt = db.select(Card).order_by(Card.priority, Card.title)
+    # stmt = db.select(Card).where(Card.status == 'To Do')
+    stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
     cards = db.session.scalars(stmt)
-    return jsonify(cards)
+    return CardSchema(many=True).dump(cards)
     # for card in cards:
     #     print(card.title, card.priority)
     # print(cards)
     # print(cards[0].__dict__)
 
 
-@app.cli.command('first_cards')
-def first_cards():
+@app.cli.command('first_card')
+def first_card():
     # select * from cards limit 1;
-    # cards = Card.query.first()
+    # card = Card.query.first()
     stmt = db.select(Card).limit(1)
-    cards = db.session.scalar(stmt)
-    print(cards.__dict__)
+    card = db.session.scalar(stmt)
+    print(card.__dict__)
 
 
 @app.cli.command('count_ongoing')
 def count_ongoing():
     stmt = db.select(db.func.count()).select_from(
-        Card).filter_by(status='ongoing')
+        Card).filter_by(status='Ongoing')
     cards = db.session.scalar(stmt)
     print(cards)
 
@@ -110,3 +142,122 @@ def count_ongoing():
 @app.route('/')
 def index():
     return "Hello World!"
+
+# from flask import Flask, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import date
+# from flask_marshmallow import Marshmallow
+# # from symbol import stmt
+
+# app = Flask(__name__)
+# app.config['JSON_SORT_KEYS'] = False
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
+
+# db = SQLAlchemy(app)
+# ma = Marshmallow(app)
+
+
+# class Card(db.Model):
+#     __tablename__ = 'cards'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100))
+#     description = db.Column(db.Text)
+#     date = db.Column(db.Date)
+#     status = db.Column(db.String)
+#     priority = db.Column(db.String)
+
+
+# class CardSchema(ma.Schema):
+#     class Meta:
+#         fields = ('id', 'title', 'description', 'status', 'priority', 'date')
+
+# # Define a custom CLI (terminal) command
+
+
+# @app.cli.command('create')
+# def create_db():
+#     db.create_all()
+#     print("Tables created")
+
+
+# @app.cli.command('drop')
+# def drop_db():
+#     db.drop_all()
+#     print("Tables dropped")
+
+
+# @app.cli.command('seed')
+# def seed_db():
+#     cards = [
+#         Card(
+#             title='Start the project',
+#             description='Stage 1 - Create the database',
+#             status='To Do',
+#             priority='High',
+#             date=date.today()
+#         ),
+#         Card(
+#             title="SQLAlchemy",
+#             description="Stage 2 - Integrate ORM",
+#             status="Ongoing",
+#             priority="High",
+#             date=date.today()
+#         ),
+#         Card(
+#             title="ORM Queries",
+#             description="Stage 3 - Implement several queries",
+#             status="Ongoing",
+#             priority="Medium",
+#             date=date.today()
+#         ),
+#         Card(
+#             title="Marshmallow",
+#             description="Stage 4 - Implement Marshmallow to jsonify models",
+#             status="Ongoing",
+#             priority="Medium",
+#             date=date.today()
+#         )
+#     ]
+
+#     db.session.add_all(cards)
+#     db.session.commit()
+#     print('Tables seeded')
+
+
+# @app.cli.command('/cards/')
+# def all_cards():
+#     # select * from cards;
+#     # cards = Card.query.all()
+#     # stmt = db.select(Card).where(db.or_
+#     #                              (Card.status == 'To do', Card.priority == 'High'))
+#     stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+#     cards = db.session.scalars(stmt)
+#     return CardSchema(many=True).dump(cards)
+#     # for card in cards:
+#     #     print(card.title, card.priority)
+#     # print(cards)
+#     # print(cards[0].__dict__)
+
+
+# @app.cli.command('first_cards')
+# def first_cards():
+#     # select * from cards limit 1;
+#     # cards = Card.query.first()
+#     stmt = db.select(Card).limit(1)
+#     cards = db.session.scalar(stmt)
+#     print(cards.__dict__)
+
+
+# @app.cli.command('count_ongoing')
+# def count_ongoing():
+#     stmt = db.select(db.func.count()).select_from(
+#         Card).filter_by(status='ongoing')
+#     cards = db.session.scalar(stmt)
+#     print(cards)
+
+
+# @app.route('/')
+# def index():
+#     return "Hello World!"
